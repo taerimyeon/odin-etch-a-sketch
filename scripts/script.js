08fa5168e879db2c11938415eb319750fb9a3acc
Steven Jonathan <48115797+taerimyeon@users.noreply.github.com>
2024-09-07 18:28:03 +0800

    Added Click Handler for Etching the Sketch

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const GRID_WIDTH = 20; // In pixel
 const GRID_HEIGHT = 20; // In pixel
 const GRID_OVERLAY_COLOR = "#00FFFF"; // Cyan
+const GRID_BACKGROUND_COLOR = "#000000" // Black
+let GRID_DATA = {}; // Stores the grid information (to determine if a grid is already clicked or not)
 
 function generateGrid(width, height) {
   let containerElement = document.getElementById("container");
@@ -10,8 +12,10 @@ function generateGrid(width, height) {
       rowElement.classList.add("container-row");
       for(let cols = 0; cols < height; cols++) {
         const gridElement = createGrid();
+        gridElement.id = `${rows}-${cols}`;
         gridElement.addEventListener("mouseenter", setOverlay);
         gridElement.addEventListener("mouseleave", unsetOverlay);
+        gridElement.addEventListener("click", setColor);
         rowElement.appendChild(gridElement);
       }
       containerElement.appendChild(rowElement);
@@ -24,7 +28,18 @@ function setOverlay(event) {
 }
 
 function unsetOverlay(event) {
-  event.target.style.backgroundColor = "";
+  if (GRID_DATA[event.target.id] !== undefined) {
+    event.target.style.backgroundColor = GRID_DATA[event.target.id].backgroundColor;
+  } else {
+    event.target.style.backgroundColor = "";
+  }
+}
+
+function setColor(event) {
+  GRID_DATA[event.target.id] = {
+    backgroundColor: GRID_BACKGROUND_COLOR
+  };
+  event.target.style.backgroundColor = GRID_DATA[event.target.id].backgroundColor;
 }
 
 function createGrid() {
